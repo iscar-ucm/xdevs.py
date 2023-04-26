e4b0f44c621dfc5d7541df4e9f4bf4dabad7be97
OscarFdezS <oscar.fernandez.sebastian@alumnos.upm.es>
2023-04-26 22:50:55 +0200

    TCPOutputHandler update.

--- xdevs/plugins/output_handlers/tcp_output_handler.py
+++ xdevs/plugins/output_handlers/tcp_output_handler.py
@@ -4,6 +4,7 @@ import threading
 
 from xdevs.rt_sim.output_handler import OutputHandler
 
+
 # This will be a client that subscribe to a server to send the outgoing event of the system.
 
 def clear_queue(q):
@@ -13,6 +14,7 @@ def clear_queue(q):
             q.get()
     except TypeError as e:
         print(f'Argument in clear_queue must be a queue type: {e}')
+
 
 def tcp_default_format(port, msg):
     """Default format for outgoing events."""
@@ -24,10 +26,6 @@ class TCPOutputHandler(OutputHandler):
         """
         TCPOutHandler is a socket client that sends to a server (described as host, port) the outgoing events of the
         system. By default, the events are in the form: port, msg.
-
-        TODO:  si el handler se ha conectado mas tarde al servidor enviara toda la cola de golpe.
-                posibles soluciones:    1) poner una espera para que se envie uno por linea
-                                        2) borrar toda la cola anterior. <- OFS apoya esta
 
         :param str host: is the IP of the device where the server is hosted. Default is 'LocalHost'.
         :param int port: is the port in which the host is listening.
@@ -52,24 +50,35 @@ class TCPOutputHandler(OutputHandler):
 
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.lock = None
+        self.is_connected = False
 
     def exit(self):
         print(f'CLosing client to server {self.host} in port {self.port}...')
-        self.lock = False
         self.client_socket.close()
+        self.is_connected = False
 
     def run(self):
-        self.lock = True
         while True:
-            # First we connect to the server.
+            # First we check for outgoing events
+            port, msg = self.queue.get()
             try:
-                while self.lock:
+                if self.is_connected:
+
+                    # If an outgoing event occurs it is sent to the server ,but first it is formatted.
+                    data = self.event_parser(port, msg)
+
+                    if self.client_socket.fileno() > 0:
+                        # We can only send data if the client_socket is not close. Client_socket is closed when
+                        # .fileno() return 0
+                        self.client_socket.sendall(data.encode())
+                else:
                     try:
+
                         self.client_socket.connect((self.host, self.port))
                         print('Connected to server...')
-                        clear_queue(self.queue)     # TODO solucion 2
-                        self.lock = False
+
+                        self.is_connected = True
+
                     except ConnectionRefusedError:
                         # If the connection is refused, wait for a time t_wait and try again.
                         # This exception can be raised when: the port is blocked or closed by a firewall, host is not
@@ -77,22 +86,6 @@ class TCPOutputHandler(OutputHandler):
                         print(f'Connection refused, trying again in {self.t_wait} s. ')
                         time.sleep(self.t_wait)
 
-                # If the connection was a success.
-                # First we check for outgoing events
-                port, msg = self.queue.get()
-
-                # If an outgoing event occurs it is sent to the server ,but first it is formatted.
-
-                data = self.event_parser(port, msg)
-
-                if self.client_socket.fileno() > 0:
-                    # We can only send data if the client_socket is not close. Client_socket is closed when .fileno() return
-                    # 0
-                    self.client_socket.sendall(data.encode())
-                    # time.sleep(0.1) TODO solucion 1
-
-                print(f'msg sent: {data}')
-                
             except OSError as e:
                 # If a system error occurred when connecting, we assume that the server has been shut down.
                 print(f'Error while connecting to server: {e}')
